ccfe4446581ad6411a13b28f574630faa6c19247
Galal-20 <ga71387@gmail.com>
2026-04-16 11:17:30 -0700

    Update files

---
+++ app/src/main/java/com/example/allgoods/UI/Main/WelcomeActivity.java
@@ -0,0 +1,42 @@
+package com.example.allgoods.UI.Main;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.allgoods.Data.local.SharedPrefManager;
+import com.example.allgoods.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+
+    private static final int SPLASH_DELAY = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_welcome);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        navigateNext();
+    }
+
+    private void navigateNext() {
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }, SPLASH_DELAY);
+    }
+}
+
